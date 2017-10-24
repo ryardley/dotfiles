d1d1d83e2bdd6fb9a6c732efe849aa33fa082bb4
Luke Hamilton <luke@mettadata.com>
2017-10-24 19:46:39 +1000

    Added function for processing the *.erb files. and refactored other method to make simpler.

--- lib/components.rb
+++ lib/components.rb
@@ -1,11 +1,20 @@
+require 'erb'
 require './lib/config.rb'
 require './lib/detect_os.rb'
 
-# Run once off installers for all components listed in the config file
+# Install a component
+# *.nothing => Do nothing
+# *.sh => Do nothing. These files are for system usage.
+# *.run => Run custom install script
+# *.smylink => file get's symlinked to $HOME
+# *.zshrc => get source into system .zshrc file
 def install_components
-
   for component in components_list do
-    install_component(component)
+    puts "Installing component: #{component}"
+    process_zshrc_files(component)
+    process_script_files(component)
+    process_symlink_files(component)
+    process_erb_files(component)
   end
 end
 
@@ -14,39 +23,56 @@ def components_list
   config["#{detect_os}"]
 end
 
-# Install a component
-# *.nothing => Do nothing
-# *.sh => Do nothing. These files are for system usage.
-# *.run => Run custom install script
-# *.smylink => file get's symlinked to $HOME
-# *.zshrc => get source into system .zshrc file
-def install_component(component)
-  puts "Installing component: #{component}"
-  process_zshrc_files(component)
-  process_script_files(component)
-  process_symlink_file(component)
-end
-
 def process_zshrc_files(component)
-  # puts "No zshrc files to process" if zshrc_files(component).empty?
   for file in zshrc_files(component) do
-    add_zshrc_files_to_sys_zshrc(file)
+    file = file.sub("./","")
+    addline = "source #{Dir.pwd}/#{file}\n"
+    zshrc_file = "#{Dir.home}/.zshrc"
+    dest_file = File.readlines(zshrc_file)
+    if dest_file.grep(addline).any?
+      puts "!!! #{file} already added to #{Dir.home}/.zshrc. Doing Nothing !!!"
+    else
+      puts "Adding #{file} to #{Dir.home}/.zshrc"
+      File.open(zshrc_file, 'a') { |file| file.write(addline) }
+    end
   end
 end
 
 # A way to process general purpose scripts.
 def process_script_files(component)
-  # puts "No install scripts to run" if script_files(component).empty?
   for script in script_files(component) do
-    run_script(script)
+    puts "Running custom install script: #{script}"
+    system(script)
   end
 end
 
-def process_symlink_file(component)
-  # puts "No symlink files to link" if symlink_files(component).empty?
+def process_symlink_files(component)
   for symlink in symlink_files(component) do
-    symlink_file(symlink)
+    puts "Symlinking: #{symlink}"
+    pwd = Dir.pwd
+    sourse_file = symlink.sub("./","")
+    dest_file = symlink.split("/").pop.sub(".symlink", "")
+    cmd = "gln -srf #{pwd}/#{sourse_file} ~/.#{dest_file}"
+    system(cmd)
   end
+end
+
+def process_erb_files(component)
+  for file in erb_files(component) do
+    file_data = ERB.new(File.read(file)).result(binding)
+    dest = "#{ENV['HOME']}/.gitconfig"
+
+    if File.exist?(dest)
+      puts "File: #{dest} exists. Doing nothing. "
+    else
+      puts "Writing file: #{dest}"
+      File.write(dest, file_data) unless File.exist?(dest)
+    end
+  end
+end
+
+def erb_files(component)
+  Dir["./etc/#{component}/*.erb"]
 end
 
 def zshrc_files(component)
@@ -61,29 +87,4 @@ end
 # Return Array of *.run files in component directory (Should only be one. However there is nothing stop there being more. )
 def script_files(component)
   Dir["./etc/#{component}/*.run"]
-end
-
-# Execute script (*.run, *.rb, *.py, *.sh)
-def run_script(script)
-  puts "Running custom install script: #{script}"
-  system(script)
-end
-
-# Symlink files to home directory
-def symlink_file(symlink)
-  puts "Symlinking: #{symlink}"
-  pwd = Dir.pwd
-  sourse_file = symlink.sub("./","")
-  dest_file = symlink.split("/").pop.sub(".symlink", "")
-  cmd = "gln -srf #{pwd}/#{sourse_file} ~/.#{dest_file}"
-  system(cmd)
-end
-
-def add_zshrc_files_to_sys_zshrc(file)
-  puts "Adding #{file} to #{Dir.home}/.zshrc"
-  file = file.sub("./","")
-  addline = "source #{Dir.pwd}/#{file}\n"
-  zshrc_file = "#{Dir.home}/.zshrc"
-  dest_file = File.readlines(zshrc_file)
-  File.open(zshrc_file, 'a') { |file| file.write(addline) } unless dest_file.grep(addline).any?
 end
