23fa22814daa0f605ada3d39d352631ed6c26675
Luke Hamilton <luke@mettadata.com>
2017-10-25 00:21:52 +1000

    Removed more execes puts cmds.

--- lib/components.rb
+++ lib/components.rb
@@ -30,9 +30,9 @@ def process_zshrc_files(component)
     zshrc_file = "#{Dir.home}/.zshrc"
     dest_file = File.readlines(zshrc_file)
     if dest_file.grep(addline).any?
-      puts "!!! #{file} already added to #{Dir.home}/.zshrc. Doing Nothing !!!"
+      # puts "!!! #{file} already added to #{Dir.home}/.zshrc. Doing Nothing !!!"
     else
-      puts "Adding #{file} to #{Dir.home}/.zshrc"
+      # puts "Adding #{file} to #{Dir.home}/.zshrc"
       File.open(zshrc_file, 'a') { |file| file.write(addline) }
     end
   end
@@ -41,14 +41,14 @@ end
 # A way to process general purpose scripts.
 def process_script_files(component)
   for script in script_files(component) do
-    puts "Running custom install script: #{script}"
+    # puts "Running custom install script: #{script}"
     system(script)
   end
 end
 
 def process_symlink_files(component)
   for symlink in symlink_files(component) do
-    puts "Symlinking: #{symlink}"
+    # puts "Symlinking: #{symlink}"
     pwd = Dir.pwd
     sourse_file = symlink.sub("./","")
     dest_file = symlink.split("/").pop.sub(".symlink", "")
@@ -63,9 +63,9 @@ def process_erb_files(component)
     dest = "#{ENV['HOME']}/.gitconfig"
 
     if File.exist?(dest)
-      puts "File: #{dest} exists. Doing nothing. "
+      # puts "File: #{dest} exists. Doing nothing. "
     else
-      puts "Writing file: #{dest}"
+      # puts "Writing file: #{dest}"
       File.write(dest, file_data) unless File.exist?(dest)
     end
   end
@@ -87,5 +87,5 @@ end
 # Return Array of *.run files in component directory (Should only be one. However there is nothing stop there being more. )
 def script_files(component)
   Dir["./etc/#{component}/install_#{component}.zsh"] +
-  Dir["./etc/#{component}/install_#{component}*.rb"] 
+  Dir["./etc/#{component}/install_#{component}*.rb"]
 end
