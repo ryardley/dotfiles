7d4e58f2bded3e8860dfc77fe3a1866ac23e19cc
Luke Hamilton <luke@mettadata.com>
2017-10-26 14:04:52 +1000

    Removed brew commands as this is installed via the homebrew setup.

--- etc/zsh/install_zsh.zsh
+++ etc/zsh/install_zsh.zsh
@@ -1,14 +1,4 @@
 #!/usr/bin/env zsh
 
-brew install -v zsh
-brew install -v zsh-autosuggestions
-brew install -v zsh-completions
-brew install -v zsh-lovers
-brew install -v zsh-navigation-tools
-brew install -v zsh-syntax-highlighting
-brew install -v zsh-history-substring-search
-brew install -v liquidprompt
-brew install -v z
-
 sudo dscl . -create /Users/$USER UserShell `which zsh`
 echo "Shell set to: `dscl . -read /Users/$USER UserShell`"
