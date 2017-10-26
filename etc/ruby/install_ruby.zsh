5b1b469752479c26c9c1f5bb8ea1b129bc810686
Luke Hamilton <luke@mettadata.com>
2017-10-26 14:02:12 +1000

    Setup ruby via brew and not chruby anymore.

--- etc/ruby/install_ruby.zsh
+++ etc/ruby/install_ruby.zsh
@@ -1,16 +1,3 @@
 #!/usr/bin/env zsh
 
-SCRIPT=`realpath $0`
-SCRIPTPATH=`dirname $SCRIPT`
-
-source $SCRIPTPATH/ruby.zshrc
-
-chruby ruby-2.4
-
-if [ $(which ruby ) = /usr/bin/ruby ]; then
-  ruby-install ruby-2.4
-else
-  echo "Ruby is installed"
-fi
-
-chruby | sed 's/\*//' > ~/.ruby-version
+gem install bundle
--- etc/ruby/ruby.zshrc
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env zsh
-
-source /usr/local/opt/chruby/share/chruby/chruby.sh
-source /usr/local/opt/chruby/share/chruby/auto.sh
