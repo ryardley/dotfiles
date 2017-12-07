af47873d6f764ba6b5c720b82ced33892cde57ea
Luke Hamilton <luke@mettadata.com>
2017-12-07 23:04:33 +1000

    Finished the local repo status checker.

--- etc/bin/dotfiles_status.zsh
+++ etc/bin/dotfiles_status.zsh
@@ -1,8 +1,27 @@
 #!/usr/bin/env zsh
 
+SCRIPT=`realpath $0`
+SCRIPTPATH=`dirname $SCRIPT`
+
+cd $DOTFILES
 # Check if loacl repo is clean
 STATUS=`git status -s`
 
 if [ $STATUS ]; then
-  echo "!! WARNING: You have uncommited files in your loacl .dotfiles repo. !!"
+  echo "********************** RUNNING LOCAL STATUS CHECKER ******************"
+  echo "!! WARNING: You have uncommited files in your local .dotfiles repo. !!"
+  echo "!!   Please commit and contribute your changes back to upstream     !!"
+  echo "!!                            Thank you                             !!"
+  echo "**********************************************************************"
+  echo
+
+  # Create a dirty marker
+  touch $SCRIPTPATH/../../var/DIRTY_REPO
+  export DIRTY=true
+else
+  # Remove the dirty marker
+  rm $SCRIPTPATH/../../var/DIRTY_REPO
+  export DIRTY=false
 fi
+cd - > /dev/null
+
