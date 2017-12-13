17ea1725ac0e391514a900bc308d4c70c1eb355b
Luke Hamilton <luke@mettadata.com>
2017-12-13 17:56:15 +1000

    Removed due to rename.

--- etc/dotfiles/install_dotfiles.zsh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env zsh
-UPSTREAM=`git remote | grep upstream`
-
-if [ ! $UPSTREAM ]; then
-  git remote add upstream git@github.com:MettaData/dotfiles.git
-  echo "Added upstream repo locally"
-fi
