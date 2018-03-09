95b571890cd08ebc4798828b2dc2fd4f69ba0a5a
Rudi Yardley <contact@rudiyardley.com>
2018-03-10 10:35:38 +1100

    Added installer script

---
+++ installer.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+if [ ! -d "/Library/Developer/CommandLineTools" ]; then
+  echo "=============================="
+  echo "Installing command line tools"
+  echo "============================="
+  echo "Please follow the prompts to install command line tools and try running this script again"
+  xcode-select --install
+  exit 0
+fi
+echo "Command line tools installed"
+echo ""
+echo "=============================="
+echo " Generating SSH KEYS"
+echo "=============================="
+## TODO make use dynamic input
+ssh-keygen -t rsa -b 4096 -C "contact@rudiyardley.com"
+open "https://github.com"
+echo ""
+echo ""
+echo "Log into github and copy the following deploy key to your account on Github:"
+echo ""
+cat ~/.ssh/id_rsa.pub
+echo ""
+echo ""
+echo "Press a key when ready..."
+read
+echo "And once more to be sure..."
+read
+
+echo "Cloning dotfiles repo"
+git clone git@github.com:ryardley/dotfiles.git ~/.dotfiles && cd ~/.dotfiles && rake install
